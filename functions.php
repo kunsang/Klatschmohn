f92907d6b152920470a682f4647b2574a076f40d
Kunsang <kunsang.tsering@gmail.com>
2014-04-26 20:53:56 +0200

    ansprechpartner css

--- functions.php
+++ functions.php
@@ -177,3 +177,30 @@ $query->set('cat', '-15 -17');
 return $query;
 }
 add_filter('pre_get_posts', 'exclude_category');
+
+
+// always paste as plain text
+//
+// https://gist.github.com/tillkruess/5425656
+
+// foreach ( array( 'tiny_mce_before_init', 'teeny_mce_before_init') as $filter ) {
+// 	add_filter( $filter, function( $mceInit ) {
+// 		$mceInit[ 'paste_text_sticky' ] = true;
+// 		$mceInit[ 'paste_text_sticky_default' ] = true;
+// 		return $mceInit;
+// 	});
+// }
+
+// // load 'paste' plugin in minimal/pressthis editor
+// add_filter( 'teeny_mce_plugins', function( $plugins ) {
+// 	$plugins[] = 'paste';
+// 	return $plugins;
+// });
+
+// // remove "Paste as Plain Text" button from editor
+// add_filter( 'mce_buttons_2', function( $buttons ) {
+// 	if( ( $key = array_search( 'pastetext', $buttons ) ) !== false ) {
+// 		unset( $buttons[ $key ] );
+// 	}
+// 	return $buttons;
+// });
